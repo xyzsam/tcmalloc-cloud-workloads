c90030c338bb545c849e32bc8f669bec5de69927
Sam Xi <samxi@seas.harvard.edu>
2016-08-10 15:47:59 -0400

    Use linker script when linking against tcmalloc.
    Also, use sized deletes.

--- xapian/xapian-core-1.4.0/build/do_configure.sh
+++ xapian/xapian-core-1.4.0/build/do_configure.sh
@@ -5,7 +5,7 @@
 
 TCMALLOC_BUILD=${1:-baseline}
 
-USAGE="Usage: $0 [baseline (default) | all_magic | all_with_samples]"
+USAGE="Usage: $0 [baseline (default) | all_magic]"
 
 INSTALL_SUFFIX=""
 if [ "${TCMALLOC_BUILD}" == "baseline" ]
@@ -13,10 +13,7 @@ then
   INSTALL_SUFFIX=""
 elif [ "${TCMALLOC_BUILD}" == "all_magic" ]
 then
-  INSTALL_SUFFIX="_all_magic_no_samples"
-elif [ "${TCMALLOC_BUILD}" == "all_magic_with_samples" ]
-then
-  INSTALL_SUFFIX="_all_magic_with_samples"
+  INSTALL_SUFFIX="_all_magic"
 else
   echo $USAGE
 fi
@@ -24,6 +21,8 @@ fi
 LIBTCMALLOC_DIR=/home/${USER}/gperftools/install${INSTALL_SUFFIX}
 CXXABI_FLAGS="-fabi-version=2 -D_GLIBCXX_USE_CXX11_ABI=0"
 TCMALLOC_SAFETY_FLAGS="-fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free"
+SIZED_DELETE_FLAGS="-fsized-deallocation"
+MAGIC_LINKER_SCRIPT="-T ${LIBTCMALLOC_DIR}/spec_linker.ld"
 
 INSTALL_DIR=/home/${USER}/xapian/install
 
@@ -32,8 +31,8 @@ INSTALL_DIR=/home/${USER}/xapian/install
              --disable-shared \
              CC=/group/vlsiarch/skanev/toolchain_6_1/bin/gcc \
              CXX=/group/vlsiarch/skanev/toolchain_6_1/bin/g++ \
-             CFLAGS="-I/home/libunwind/install/include -g -O3 ${TCMALLOC_SAFETY_FLAGS}" \
+             CFLAGS="-I/home/libunwind/install/include -g -O3 ${TCMALLOC_SAFETY_FLAGS} ${SIZED_DELETE_FLAGS}" \
              CPPFLAGS="-I/home/skanev/libunwind/install/include -I${INSTALL_DIR}/include" \
-             CXXFLAGS="-I/home/libunwind/install/include -g -std=c++1y -O3 ${CXXABI_FLAGS} ${TCMALLOC_SAFETY_FLAGS} " \
+             CXXFLAGS="-I/home/libunwind/install/include -g -std=c++1y -O3 ${CXXABI_FLAGS} ${TCMALLOC_SAFETY_FLAGS} ${SIZED_DELETE_FLAGS} " \
              LDFLAGS="-L/home/libunwind/install/lib -O3" \
-             LIBS="${INSTALL_DIR}/lib/libuuid.a ${LIBTCMALLOC_DIR}/lib/libtcmalloc.a /home/skanev/libunwind/install/lib/libunwind.a -pthread -lrt"
+             LIBS="${MAGIC_LINKER_SCRIPT} ${INSTALL_DIR}/lib/libuuid.a ${LIBTCMALLOC_DIR}/lib/libtcmalloc.a /home/skanev/libunwind/install/lib/libunwind.a -pthread -lrt"
