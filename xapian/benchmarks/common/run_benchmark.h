83e2fbf675bd6cbc0077b0d16b0b914917761dc1
Sam Xi <samxi@seas.harvard.edu>
2016-08-13 16:08:11 -0400

    Reduce benchmark iterations to 5.

--- xapian/benchmarks/common/run_benchmark.h
+++ xapian/benchmarks/common/run_benchmark.h
@@ -1,7 +1,7 @@
 #ifndef _RUN_BENCHMARK_H_
 #define _RUN_BENCHMARK_H_
 
-#define NUM_ITERATIONS 10
+#define NUM_ITERATIONS 5
 
 void benchmark_setup(Xapian::Database &db, Xapian::Enquire &enquire,
                      Xapian::QueryParser &qparser);
