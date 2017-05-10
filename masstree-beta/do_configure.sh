cbcf0193ced7a16008f6e1bda6cfa139b08ac703
Sam Xi <samxi@seas.harvard.edu>
2017-05-10 15:17:02 -0400

    Add command line option to use magic tcmalloc build.

--- masstree-beta/do_configure.sh
+++ masstree-beta/do_configure.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+TCMALLOC_BUILD=${1:-baseline}
+
+USAGE="Usage: $0 [baseline (default) | all_magic]"
+
+INSTALL_SUFFIX=""
+if [ "${TCMALLOC_BUILD}" == "baseline" ]
+then
+  INSTALL_SUFFIX=""
+elif [ "${TCMALLOC_BUILD}" == "all_magic" ]
+then
+  INSTALL_SUFFIX="_all_magic"
+else
+  echo $USAGE
+fi
+
 TOOLCHAIN_HOME=/usr
 LIBUNWIND_HOME=/usr/local
 LIBSTDCPP_HOME=/usr/lib/gcc/x86_64-linux-gnu/6
 
-LIBTCMALLOC_HOME=/home/${USER}/gperftools/install
-MAGIC_LINKER_SCRIPT="-T /home/${USER}/gperftools/install/spec_linker.ld"
+LIBTCMALLOC_HOME=/home/${USER}/gperftools/install${INSTALL_SUFFIX}
+MAGIC_LINKER_SCRIPT="-T ${LIBTCMALLOC_HOME}/spec_linker.ld"
 ABI_FLAGS="-fabi-version=2 -D_GLIBCXX_USE_CXX11_ABI=0"
 
 ./configure CC=${TOOLCHAIN_HOME}/bin/gcc \
