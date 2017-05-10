acbb2b96a4ef63eb0af871720be27d44a3515ff4
Sam Xi <samxi@seas.harvard.edu>
2017-05-10 14:48:50 -0400

    Fix Masstree do_configure.sh.

--- masstree-beta/do_configure.sh
+++ masstree-beta/do_configure.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+TOOLCHAIN_HOME=/usr
+LIBUNWIND_HOME=/usr/local
+LIBSTDCPP_HOME=/usr/lib/gcc/x86_64-linux-gnu/6
+
 LIBTCMALLOC_HOME=/home/${USER}/gperftools/install
-LIBUNWIND_HOME=/home/skanev/libunwind/install
-LIBSTDCPP_HOME=/group/vlsiarch/skanev/toolchain_6_1/
-MAGIC_LINKER_SCRIPT="-T /home/${USER}/gperftools/install_all_magic/spec_linker.ld"
+MAGIC_LINKER_SCRIPT="-T /home/${USER}/gperftools/install/spec_linker.ld"
 ABI_FLAGS="-fabi-version=2 -D_GLIBCXX_USE_CXX11_ABI=0"
 
-./configure CC=/group/vlsiarch/skanev/toolchain_6_1/bin/gcc \
-            CXX=/group/vlsiarch/skanev/toolchain_6_1/bin/g++ \
-            CXXFLAGS="-g -W -Wall -O3 -fsized-deallocation ${ABI_FLAGS}" \
+./configure CC=${TOOLCHAIN_HOME}/bin/gcc \
+            CXX=${TOOLCHAIN_HOME}/bin/g++ \
+            CXXFLAGS="-g -W -Wall -O3 -std=c++14 -fsized-deallocation ${ABI_FLAGS}" \
             LDFLAGS="-static"\
-            LIBS="${MAGIC_LINKER_SCRIPT} ${LIBTCMALLOC_HOME}/lib/libtcmalloc.a ${LIBUNWIND_HOME}/lib/libunwind.a ${LIBSTDCPP_HOME}/lib64/libstdc++.a"
+            LIBS="${MAGIC_LINKER_SCRIPT} ${LIBTCMALLOC_HOME}/lib/libtcmalloc.a ${LIBUNWIND_HOME}/lib/libunwind.a ${LIBSTDCPP_HOME}/libstdc++.a"
